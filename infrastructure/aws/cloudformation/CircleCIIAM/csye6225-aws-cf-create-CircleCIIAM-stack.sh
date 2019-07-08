bec97aab5f1c4a762dcf178d7b4b1a030d5d38ff
cyrilsebastian1811 <sebastian.c@northeastern.edu>
2019-07-07 20:49:44 -0400

    CircleCIIAM cloud formation

--- infrastructure/aws/cloudformation/CircleCIIAM/csye6225-aws-cf-create-CircleCIIAM-stack.sh
+++ infrastructure/aws/cloudformation/CircleCIIAM/csye6225-aws-cf-create-CircleCIIAM-stack.sh
@@ -1,50 +1,33 @@
 #!/bin/bash
 
-echo "enter the name for your Virtual Private Cloud stack(alphanumeric)"
+echo "Enter the name for your CircleCI Roles and Policies Stack(alphanumeric)"
 read stackName
-
-aws cloudformation describe-stacks --stack-name $stackName &> /dev/null
-
-if [ $? -eq 0 ]
-then
-	echo "Failed: stack by this name already exist"
-	exit
-fi
-
 if [ -z "$stackName" ]
 then
     echo "Failed: enter a stack name"
     exit
 fi
 
-echo "enter the CidrBlock you wish for your VPC, should be of the format - 10.0.0.0/16"
-read vpcCidrBlock
-if [ -z "$vpcCidrBlock" ]
+echo "enter your domainName.csye6225.com for s3 bucket"
+read domainName
+if [ -z "$domainName" ]
 then
-    echo "Failed: enter a CidrBlock representation for you VPC"
+    echo "Failed: enter your domain name, which will be usedd to create a S3 bucket"
     exit
 fi
 
-
-echo "enter the CidrBits of the Subnet between 4-16"
-read subnetCidrBits
-if [ -n $subnetCidrBits ]
+aws cloudformation describe-stacks --stack-name $stackName &> /dev/null
+if [ $? -eq 0 ]
 then
-    if [[ ( $subnetCidrBits -gt 16 ) || ( $subnetCidrBits -lt 4 ) ]]
-    then
-        exit
-    fi
-else
-    echo "cidrBits cant be null"
-    exit
+	echo "Failed: stack by this name already exist"
+	exit
 fi
-
 
 status=$(aws cloudformation create-stack --stack-name $stackName \
---template-body file://csye6225-cf-networking.json --parameters \
-ParameterKey=vpcCidrBlock,ParameterValue=$vpcCidrBlock \
-ParameterKey=subnetCidrBits,ParameterValue=$subnetCidrBits \
---on-failure DELETE)
+--template-body file://csye6225-cf-CircleCIIAM.json \
+--parameters \
+ParameterKey=domainName,ParameterValue=$domainName \
+--capabilities CAPABILITY_NAMED_IAM --on-failure DELETE)
 if [ $? -eq 0 ]
 then
     echo "please wait....."
