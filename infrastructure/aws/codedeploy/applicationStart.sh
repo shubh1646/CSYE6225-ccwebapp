1a34f376a5b2a63e80ac1ff71bac2080f9b8f668
mansigandhi1994 <gandhi.man@husky.neu.edu>
2019-07-26 18:25:37 -0400

    Changes

--- infrastructure/aws/codedeploy/applicationStart.sh
+++ infrastructure/aws/codedeploy/applicationStart.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
 #Starting tomcat service
-sudo /opt/aws/amazon-cloudwatch-agent/bin/amazon-cloudwatch-agent-ctl \
-    -a fetch-config \
-    -m ec2 \
-    -c file:/opt/cloudwatch-config.json \
-    -s
+#sudo /opt/aws/amazon-cloudwatch-agent/bin/amazon-cloudwatch-agent-ctl \
+#    -a fetch-config \
+ #   -m ec2 \
+#    -c file:/opt/cloudwatch-config.json \
+#    -s
 
 sudo systemctl start tomcat.service
 # sudo systemctl status tomcat
