52a6c319059abec86700dcc5e70b80f6ab8ebf15
cyrilsebastian1811 <sebastian.c@northeastern.edu>
2019-08-08 17:07:19 -0400

    auto-scaling stack changes

--- infrastructure/aws/cloudformation/waf/csye6225-aws-cf-create-applicattion-waf-stack.sh
+++ infrastructure/aws/cloudformation/waf/csye6225-aws-cf-create-applicattion-waf-stack.sh
@@ -1,33 +1,17 @@
 #!/bin/bash
 
 echo "enter the name for your Application Stack(alphanumeric)"
-read applicationStackName
+read applicationWAFStackName
 echo "enter the name of your Virtual Private Cloud (networking) stack(alphanumeric)"
 read NetworkStackName
-echo "enter the name of your CircleCI Roles and Policies stack(alphanumeric)"
-read CircleCIStackName
-if [ -z "$applicationStackName" ] || [ -z "$NetworkStackName" ] || [ -z "$CircleCIStackName" ]
+if [ -z "$applicationWAFStackName" ] || [ -z "$NetworkStackName" ]
 then
-    echo "Failed: either Application Stack Name or Networking Stack Name or CircleCI Stack Name Null"
+    echo "Failed: either Application Stack Name or Networking Stack Name"
     exit
 fi
 
-echo "enter the ami id"
-read amiId
-if [ -z "$amiId" ]
-then
-    echo "Failed: entered amiId is Null"
-    exit
-fi
-echo "enter your domain name"
-read domainName
-if [ -z "$domainName" ]
-then
-    echo "Failed: entered domainName is Null"
-    exit
-fi
 
-aws cloudformation describe-stacks --stack-name $applicationStackName &> /dev/null
+aws cloudformation describe-stacks --stack-name $applicationWAFStackName &> /dev/null
 if [ $? -eq 0 ]
 then
    echo "Failed: Stack by this name already exist"
@@ -39,12 +23,6 @@ then
    echo "Failed: Your Virtual Private Cloud Stack not setup"
    exit
 fi
-aws cloudformation describe-stacks --stack-name $CircleCIStackName &> /dev/null
-if [ $? -ne 0 ]
-then
-   echo "Failed: Your CircleCI Stack not setup"
-   exit
-fi
 
 certificateArn=$(aws acm list-certificates --output text --query 'CertificateSummaryList[0].CertificateArn' 2> /dev/null)
 if [ $? -ne 0 ]
@@ -53,6 +31,7 @@ then
     exit
 fi
 hostedZone=$(aws route53 list-hosted-zones --output text --query 'HostedZones[0].Id' 2> /dev/null)
+# echo "${hostedZone}"
 if [ $? -ne 0 ]
 then
     echo "Failure: could not get Hosted Zones Corresponding to your domain"
@@ -61,21 +40,18 @@ fi
 hostedZoneID=${hostedZone:12}
 echo $hostedZoneID
 
-status=$(aws cloudformation create-stack --stack-name $applicationStackName \
---template-body file://csye6225-cf-auto-scaling-application.json \
+status=$(aws cloudformation create-stack --stack-name $applicationWAFStackName \
+--template-body file://csye6225-cf-application-waf.json \
 --parameters \
 ParameterKey=NetworkStackName,ParameterValue=$NetworkStackName \
-ParameterKey=amiId,ParameterValue=$amiId \
-ParameterKey=CircleCIStackName,ParameterValue=$CircleCIStackName \
-ParameterKey=domainName,ParameterValue=$domainName \
 ParameterKey=certificateArn,ParameterValue=$certificateArn \
 ParameterKey=hostedZoneID,ParameterValue=$hostedZoneID \
---capabilities CAPABILITY_NAMED_IAM --on-failure DELETE)
+--capabilities CAPABILITY_NAMED_IAM)
 
 if [ $? -eq 0 ]
 then
     echo "please wait....."
-    aws cloudformation wait stack-create-complete --stack-name $applicationStackName
+    aws cloudformation wait stack-create-complete --stack-name $applicationWAFStackName
     if [ $? -eq 0 ]
     then
         echo "Successfully setup the stack"
